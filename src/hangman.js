b4dbe674187f59e593ead327522524b5a8372b66
Sven Matic <sven.matic6@gmail.com>
2022-07-09 22:56:20 +0200

    formating code

--- src/hangman.js
+++ src/hangman.js
@@ -1,6 +1,6 @@
 import { getQuoteMaskForCharacter } from ".//get_quote_mask_for_char.js";
-import {is_a_letter} from ".//is_a_letter";
-import {uniqueCharacters} from ".//unique_characters.js";
+import { is_a_letter } from ".//is_a_letter";
+import { uniqueCharacters } from ".//unique_characters.js";
 
 let quote = {
     'text': '',
@@ -26,31 +26,31 @@ restart_btn.addEventListener("click", restart_handler)
 let paragraph = document.createElement("pre")
 paragraph.setAttribute("id", "paragraph")
 fech_quote()
-function fech_quote(){
+function fech_quote() {
     fetch("http://api.quotable.io/random").then(res => res.json())
-    .then(data => {
-        
-        setup_quote(data)
-        player.name = localStorage.getItem('name')
-        console.log(player.name)
-        console.log(quote.text)
-        run()
-    })
+        .then(data => {
+
+            setup_quote(data)
+            player.name = localStorage.getItem('name')
+            console.log(player.name)
+            console.log(quote.text)
+            run()
+        })
 }
 
 
-function setup_quote(data){
+function setup_quote(data) {
     quote.text = data.content
     quote.quoteId = data._id
     quote.length = data.length
     quote.uniqueCharacters = uniqueCharacters(quote.text)
 }
 
-function run(){
-    for (let c of quote.text){
-        if (is_a_letter(c)){
+function run() {
+    for (let c of quote.text) {
+        if (is_a_letter(c)) {
             game.visable_letters += "_ "
-        } else{
+        } else {
             game.visable_letters += c
         }
     }
@@ -59,46 +59,46 @@ function run(){
 }
 
 
-function check_if_finished(){
-    if (!game.visable_letters.includes("_")){
+function check_if_finished() {
+    if (!game.visable_letters.includes("_")) {
         return true
-    } else{
+    } else {
         return false
     }
 }
 
-function render(){
+function render() {
     paragraph.textContent = ""
     paragraph.textContent += game.visable_letters
     el.appendChild(paragraph)
 }
 
 
-function submit_letter_handler(){
+function submit_letter_handler() {
     let selected_letter = document.getElementById("letter").value
     document.getElementById("letter").value = ""
-    if (!game.guesed_letters.includes(selected_letter)){
+    if (!game.guesed_letters.includes(selected_letter)) {
         game.guesed_letters.push(selected_letter)
     }
     let new_visable_letters = ""
-    if (!quote.text.includes(selected_letter.toLowerCase()) && !quote.text.includes(selected_letter.toUpperCase())){
-        player.errors ++
+    if (!quote.text.includes(selected_letter.toLowerCase()) && !quote.text.includes(selected_letter.toUpperCase())) {
+        player.errors++
 
         var err = document.getElementById("error_num")
         err.textContent = ""
         err.textContent = "Number of errors:" + player.errors
         el.appendChild(err)
     }
-    for (let c of quote.text){
+    for (let c of quote.text) {
         new_visable_letters += getQuoteMaskForCharacter(c, game.guesed_letters);
     }
     let used_letters = ""
-    for (let c of game.guesed_letters){
+    for (let c of game.guesed_letters) {
         used_letters += c
     }
     console.log(player.errors)
     game.visable_letters = new_visable_letters
-    if (check_if_finished()){
+    if (check_if_finished()) {
         game.duration = Date.now() - start;
         console.log(game.duration)
         localStorage.setItem("game", JSON.stringify(game))
@@ -111,7 +111,7 @@ function submit_letter_handler(){
     render(new_visable_letters)
 }
 
-function restart_handler(){
+function restart_handler() {
     document.getElementById('used_letters').innerHTML = ''
     game.guesed_letters = []
     quote.text = ""
--- src/score_page.js
+++ src/score_page.js
@@ -1,28 +1,28 @@
 const game = JSON.parse(localStorage.getItem("game"))
 const player = JSON.parse(localStorage.getItem("player"))
 const quote = JSON.parse(localStorage.getItem("quote"))
-const package = {"quoteId": quote.quoteId, "length": quote.length, "uniqueCharacters": quote.uniqueCharacters, "userName": player.userName, "errors": player.errors, "duration": game.duration}
+const package = { "quoteId": quote.quoteId, "length": quote.length, "uniqueCharacters": quote.uniqueCharacters, "userName": player.userName, "errors": player.errors, "duration": game.duration }
 
 fetch("https://my-json-server.typicode.com/stanko-ingemark/hang_the_wise_man_frontend_task/highscores", {
-    method: "POST", 
-    body: JSON.stringify(package)
-  }).then(res => {
-    console.log("Request complete! response:", res);
-  });
+  method: "POST",
+  body: JSON.stringify(package)
+}).then(res => {
+  console.log("Request complete! response:", res);
+});
 
 highscores = [[player.name, 100 / (player.errors + 1)]]
 fetch("https://my-json-server.typicode.com/stanko-ingemark/hang_the_wise_man_frontend_task/highscores").then(res => res.json())
   .then(data => {
-        for([key, val] of Object.entries(data)) {
-            highscores.push([val.userName, Math.round(100 / (val.errors + 1))])
-        }
-        highscores.sort((a, b) => b[1] - a[1])
-        var score_board = document.getElementById("score_board")
-        for (let e of highscores){
-            var entry = document.createElement('li')
-            entry.appendChild(document.createTextNode(e))
-            score_board.appendChild(entry)
-        }
+    for ([key, val] of Object.entries(data)) {
+      highscores.push([val.userName, Math.round(100 / (val.errors + 1))])
+    }
+    highscores.sort((a, b) => b[1] - a[1])
+    var score_board = document.getElementById("score_board")
+    for (let e of highscores) {
+      var entry = document.createElement('li')
+      entry.appendChild(document.createTextNode(e))
+      score_board.appendChild(entry)
+    }
   })
 
 
